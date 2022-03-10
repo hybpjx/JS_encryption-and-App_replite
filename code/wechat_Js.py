f5f14678cee666348a65e167541ad70a6b4c4ed3
hybpjx <1368946902@qq.com>
2022-03-10 18:33:13 +0800

    3.10commit

---
+++ code/wechat_Js.py
@@ -0,0 +1,11 @@
+"""
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 #
+# @Time    : 2022/3/10 18:32
+# @Author  : zicliang
+# @Email   : hybpjx@163.com
+# @File    : wechat_Js.py
+# @Software: PyCharm
+"""
+
