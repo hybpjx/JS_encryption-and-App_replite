66aaa4210145ff0a367602712c22eacdb99f7622
hybpjx <1368946902@qq.com>
2022-04-06 18:17:14 +0800

    2022.4.6commit

--- replite_js_enctyption/md5.py
+++ replite_js_enctyption/md5.py
@@ -8,4 +8,11 @@
 # @File    : md5.py
 # @Software: PyCharm
 """
+from hashlib import md5
 
+print(md5(b"http://blog.csdn.net/iamhuanggua/article/details/53104838/").hexdigest())
+
+
+
+# d25e88a933a657a2a99c2f6ccb665345
+# 6f60c2e6af664c2663efbe4293666b9b
